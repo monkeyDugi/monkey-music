e0b79bac75aecdf1f96b1cbfb71f4dd625d89aac
monkeyDugi <lbd4946@gmail.com>
2020-02-27 23:41:55 +0900

    melon과 youtubeAPI Data 결합

---
+++ src/main/java/com/dugi/monkey/crowling/RequestDailyChartsDto.java
@@ -0,0 +1,33 @@
+package com.dugi.monkey.crowling;
+
+import com.dugi.monkey.domain.music.DailyCharts;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+public class RequestDailyChartsDto {
+    private String rank;
+    private String title;
+    private String singer;
+    private String image;
+    private String videoId;
+
+    @Builder
+    public RequestDailyChartsDto(String rank, String title, String singer, String image, String videoId) {
+        this.rank = rank;
+        this.title = title;
+        this.singer = singer;
+        this.image = image;
+        this.videoId = videoId;
+    }
+
+    public DailyCharts toEntity() {
+        return DailyCharts.builder()
+                .rank(rank)
+                .title(title)
+                .singer(singer)
+                .image(image)
+                .videoId(videoId)
+                .build();
+    }
+}
