fa82e87eb5d609a77047ac574190c7b5a76ee9b0
monkeyDugi <lbd4946@gmail.com>
2020-02-27 23:44:25 +0900

    quarts 스케줄러 서버 구동 시 바로 시작

--- src/main/java/com/dugi/monkey/Application.java
+++ src/main/java/com/dugi/monkey/Application.java
@@ -1,5 +1,7 @@
 package com.dugi.monkey;
 
+import com.dugi.monkey.scheduler.SchedulerTest;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +9,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 // 2. 이것이 위치한 곳 부터 설정을 읽기 때문에 항상 프로젝트 최상단에 위치한다.
 @SpringBootApplication
 public class Application {
+
+    @Autowired
+    private SchedulerTest scheduler;
+
     public static void main(String[] args) {
         // SpringApplication.run : 내장 WAS(톰캣)
         SpringApplication.run(Application.class, args);
