b097625e84027b8ea44ce3b5f0bbcc5918381044
monkeyDugi <lbd4946@gmail.com>
2020-03-01 21:34:04 +0900

    공백 정리

--- src/main/java/com/dugi/monkey/domain/music/service/DailyChartsService.java
+++ src/main/java/com/dugi/monkey/domain/music/service/DailyChartsService.java
@@ -14,6 +14,7 @@ import java.util.stream.Collectors;
 @RequiredArgsConstructor
 @Service
 public class DailyChartsService {
+
     private final DailyChartsRepository dailyChartsRepository;
 
     @Transactional(readOnly = true)
