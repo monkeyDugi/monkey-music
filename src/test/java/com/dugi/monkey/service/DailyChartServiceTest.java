55c7097ffa248609e4fc94a0cdc0b4e0dde2d567
monkeyDugi <lbd4946@gmail.com>
2020-03-04 00:18:20 +0900

    Melon, Youtub API 추가 - 기존에 Serivce에서 하던 일

---
+++ src/test/java/com/dugi/monkey/service/DailyChartServiceTest.java
@@ -0,0 +1,65 @@
+package com.dugi.monkey.service;
+
+import com.dugi.monkey.crawling.RequestDailyChartDto;
+import com.dugi.monkey.domain.music.DailyChart;
+import com.dugi.monkey.domain.music.DailyChartRepository;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.verify;
+import static org.mockito.ArgumentMatchers.any;
+
+public class DailyChartServiceTest {
+
+    @Autowired
+    private DailyChartService dailyChartService;
+
+    @Mock
+    private DailyChartRepository dailyChartRepository;
+
+    @Before
+    public void setUp() {
+        MockitoAnnotations.initMocks(this);
+        dailyChartService = new DailyChartService(dailyChartRepository);
+    }
+
+    @Test
+    public void 일간차트_리스트_불러오기() {
+
+        List<DailyChart> dailyChartsList = new ArrayList<>();
+        dailyChartsList.add(DailyChart.builder()
+                                        .rank("1")
+                                        .videoId("videoId")
+                                        .title("title")
+                                        .singer("singer")
+                                        .image("image")
+                                        .build());
+
+        given(dailyChartRepository.findAll()).willReturn(dailyChartsList);
+
+        assertThat(dailyChartService.getDailyChart().get(0).getVideoId()).isEqualTo("videoId");
+    }
+
+    @Test
+    public void 일간차트_등록() {
+        List<RequestDailyChartDto> requestDailyChartDtoList = new ArrayList<>();
+        requestDailyChartDtoList.add(RequestDailyChartDto.builder()
+                                                        .rank("1")
+                                                        .videoId("videoId")
+                                                        .title("title")
+                                                        .singer("singer")
+                                                        .image("image")
+                                                        .build());
+        dailyChartService.addDailyChart(requestDailyChartDtoList);
+
+        verify(dailyChartRepository).save(any(DailyChart.class));
+    }
+}
