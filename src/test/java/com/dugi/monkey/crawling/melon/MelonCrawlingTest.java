baced7bd370feff723b3ecaf55be60217729412b
monkeyDugi <lbd4946@gmail.com>
2020-03-07 20:10:08 +0900

    크롤링 데이터가 100개인지

---
+++ src/test/java/com/dugi/monkey/crawling/melon/MelonCrawlingTest.java
@@ -0,0 +1,31 @@
+package com.dugi.monkey.crawling.melon;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
+public class MelonCrawlingTest {
+
+    @Autowired
+    private MelonCrawling melonCrawling;
+    private List<RequestMelonCrawlingDto> melonCrawlingDtos;
+
+    @Before
+    public void setUp() {
+        melonCrawlingDtos = melonCrawling.melonCrawling();
+    }
+
+    @Test
+    public void 멜론_일간차트_TOP_100_크롤링_데이터가_100개인지() {
+        assertThat(melonCrawlingDtos.size()).isEqualTo(100);
+    }
+}
