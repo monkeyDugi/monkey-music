f773a8180bfc1c5c53dc28effe35433f6b74a109
monkeyDugi <lbd4946@gmail.com>
2020-03-07 20:48:30 +0900

    유튜브 API를 이용한 검색이 잘되는지 테스트 코드 작성

---
+++ src/test/java/com/dugi/monkey/crawling/youtube/YoutubeSearchAPIProcessingTest.java
@@ -0,0 +1,36 @@
+package com.dugi.monkey.crawling.youtube;
+
+import com.dugi.monkey.crawling.melon.MelonCrawling;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
+public class YoutubeSearchAPIProcessingTest {
+
+    @Autowired
+    private YoutubeSearchAPIProcessing youtubeSearchAPIProcessing;
+
+    @Autowired
+    private MelonCrawling melonCrawling;
+
+    private List<RequestYoutubeAPIDto> requestYoutubeAPIDtos;
+
+    @Before
+    public void setUp() throws Exception {
+        requestYoutubeAPIDtos = youtubeSearchAPIProcessing.searchDataProcessing(melonCrawling.melonCrawling());
+    }
+
+    @Test
+    public void 유튜브_검색_결과가_1개가_맞는지() {
+        assertThat(requestYoutubeAPIDtos.size()).isEqualTo(1);
+    }
+}
