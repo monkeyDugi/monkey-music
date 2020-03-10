108f4fdaa299394dcd143d1e2a263f8fdc71b2e6
monkeyDugi <lbd4946@gmail.com>
2020-03-10 16:09:27 +0900

    static으로 바로 사용하던 NONE -> import로 변경

--- src/test/java/com/dugi/monkey/crawling/melon/MelonCrawlingTest.java
+++ src/test/java/com/dugi/monkey/crawling/melon/MelonCrawlingTest.java
@@ -10,9 +10,10 @@ import org.springframework.test.context.junit4.SpringRunner;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.NONE;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
+@SpringBootTest(webEnvironment = NONE)
 public class MelonCrawlingTest {
 
     @Autowired
