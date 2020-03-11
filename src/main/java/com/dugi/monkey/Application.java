f16acc84b5917c23259d683c73b047731ca00ead
monkeyDugi <lbd4946@gmail.com>
2020-03-11 23:21:06 +0900

    스케줄러 제거(의미없는 행위였음)

--- src/main/java/com/dugi/monkey/Application.java
+++ src/main/java/com/dugi/monkey/Application.java
@@ -1,7 +1,5 @@
 package com.dugi.monkey;
 
-import com.dugi.monkey.scheduler.DailyChartScheduler;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -9,9 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 // 2. 이것이 위치한 곳 부터 설정을 읽기 때문에 항상 프로젝트 최상단에 위치한다.
 @SpringBootApplication
 public class Application {
-
-    @Autowired
-    private DailyChartScheduler scheduler;
 
     public static void main(String[] args) {
         // SpringApplication.run : 내장 WAS(톰캣)
