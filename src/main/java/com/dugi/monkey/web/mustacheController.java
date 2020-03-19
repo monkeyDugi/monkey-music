91e93c3013cfb702e5ed580d3e23bdc77e69633e
monkeyDugi <lbd4946@gmail.com>
2020-03-20 02:08:08 +0900

    검색기능 - 메인화면, 검색화면에서 queryString으로 검색

--- src/main/java/com/dugi/monkey/web/mustacheController.java
+++ src/main/java/com/dugi/monkey/web/mustacheController.java
@@ -3,6 +3,7 @@ package com.dugi.monkey.web;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class mustacheController {
@@ -13,7 +14,8 @@ public class mustacheController {
     }
 
     @GetMapping("/charts/search")
-    public String searchMusic(Model model) {
+    public String searchMusic(Model model, @RequestParam("word") String word) {
+        model.addAttribute("word", word);
         return "searchChart";
     }
 }
