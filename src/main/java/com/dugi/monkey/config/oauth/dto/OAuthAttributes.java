7ab2c03c2a8e3b9e6634a26393942a53641e466e
monkeyDugi <lbd4946@gmail.com>
2020-04-02 01:30:04 +0900

    네이버 OAuth2 사용

--- src/main/java/com/dugi/monkey/config/oauth/dto/OAuthAttributes.java
+++ src/main/java/com/dugi/monkey/config/oauth/dto/OAuthAttributes.java
@@ -28,7 +28,22 @@ public class OAuthAttributes {
 
     // OAuth2User에서 반환하는 사용자 정보는 Map이기 때문에 값 하나하나를 변환해야 함.
     public static OAuthAttributes of(String registrationId, String userNameAttributeName, Map<String, Object> attributes) {
+        if("naver".equals(registrationId)) {
+            return ofNaver("id", attributes);
+        }
+
         return ofGoogle(userNameAttributeName, attributes);
+    }
+
+    private static OAuthAttributes ofNaver(String userNameAttributeName, Map<String, Object> attributes) {
+        Map<String, Object> response = (Map<String, Object>) attributes.get("response");
+
+        return OAuthAttributes.builder()
+                .name((String) response.get("name"))
+                .email((String) response.get("email"))
+                .attributes(response)
+                .nameAttributeKey(userNameAttributeName)
+                .build();
     }
 
     private static OAuthAttributes ofGoogle(String userNameAttributeName, Map<String, Object> attributes) {
