3a9317cbd939bef0849a7a509a8c971e11f12d9b
monkeyDugi <lbd4946@gmail.com>
2020-05-08 00:17:34 +0900

    SearchChartController 테스트 코드 작성 중. 60줄에서 NPE 발생

---
+++ src/test/java/com/dugi/monkey/web/searchchart/SearchChartControllerTest.java
@@ -0,0 +1,66 @@
+package com.dugi.monkey.web.searchchart;
+
+import com.dugi.monkey.crawling.youtube.dto.ResponseYoutubeAPIDto;
+import com.dugi.monkey.service.SearchChartService;
+import com.dugi.monkey.web.searchchart.dto.ResponseSearchChartDto;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.verify;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc
+public class SearchChartControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private SearchChartService searchChartService;
+
+    @Test
+    @WithMockUser(roles = "USER")
+    public void 검색차트를_가져온다() throws Exception {
+        String word = "maroon5";
+        String email = "lbd4946@gmail.com";
+        String good = "Y";
+
+        List<ResponseSearchChartDto> responseSearchChartDtos = new ArrayList<>();
+
+        ResponseYoutubeAPIDto responseYoutubeAPIDto =
+                                            ResponseYoutubeAPIDto.builder()
+                                            .title("maroon5")
+                                            .videoId("videoId")
+                                            .image("image")
+                                            .build();
+
+        responseSearchChartDtos.add(ResponseSearchChartDto.builder().
+                responseYoutubeAPIDto(responseYoutubeAPIDto)
+                .good(good)
+                .build());
+
+        given(searchChartService.findSearchChartAll(word, email)).willReturn(responseSearchChartDtos);
+
+        mockMvc.perform(get("/api/charts/search/" + word))
+                .andExpect(status().isOk())
+                .andExpect(content().string(containsString("maroon5")));
+
+        verify(searchChartService).findSearchChartAll(word, email);
+    }
+}
