1a76b53f852b08c8fd6d304db38d5b20d43d070b
monkeyDugi <lbd4946@gmail.com>
2020-05-11 23:49:01 +0900

    마이리스트 리파지토리 테스트 작성

--- src/main/java/com/dugi/monkey/domain/music/goodchart/GoodChartRepositoryImpl.java
+++ src/main/java/com/dugi/monkey/domain/music/goodchart/GoodChartRepositoryImpl.java
@@ -9,6 +9,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
+import org.springframework.transaction.annotation.Transactional;
 
 import static com.dugi.monkey.domain.music.goodchart.QGoodChart.goodChart;
 import static com.dugi.monkey.domain.music.searchchart.QSearchChart.searchChart;
@@ -18,6 +19,7 @@ public class GoodChartRepositoryImpl implements GoodChartRepositoryCustom {
 
     private final JPAQueryFactory queryFactory;
 
+    @Transactional // update, delete는 @Transactional이 있어야 한다.
     @Override
     public Long deleteByGoodVideoId(RequestGoodChartDto requestGoodChartDto) {
         return queryFactory
