b5809d9bef06b467014d6a69a392e2416f927414
monkeyDugi <lbd4946@gmail.com>
2020-05-27 22:41:30 +0900

    apikey ignore 해제

---
+++ src/main/java/com/dugi/monkey/crawling/youtube/APIKey.java
@@ -0,0 +1,13 @@
+package com.dugi.monkey.crawling.youtube;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum APIKey {
+    
+    API_KEY("AIzaSyC8OSK4rWi-la7PbAZk3XwE2TIdIf3VY3w");
+    
+    private final String apiKey;
+}
