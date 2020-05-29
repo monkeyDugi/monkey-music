18c0014c732421fd1954702e916dd942ae0d0083
monkeyDugi <lbd4946@gmail.com>
2020-05-29 23:15:19 +0900

    오타수정

--- script/deploy.sh
+++ script/deploy.sh
@@ -5,8 +5,6 @@
 REPOSITORY=/home/ec2-user/app/monkey-music-service
 PROJECT_NAME=monkey-music
 
-cd $REPOSITORY/$PROJECT_NAME/
-
 echo "> Build 파일 복사"
 cp $REPOSITORY/zip/*.jar $REPOSITORY/
 
@@ -19,7 +17,7 @@ cp $REPOSITORY/zip/*.jar $REPOSITORY/
 echo "> 현재 구동 중인 애플리케이션 pid 확인"
 CURRENT_PID=$(pgrep -fl monkey-music | grep jar | awk '{print $1}')
 
-echo "현재 구동 중인 애플리케이션 pid : $CURRENT_PID"
+echo ">현재 구동 중인 애플리케이션 pid: $CURRENT_PID"
 
 if [ -z "$CURRENT_PID" ]; then
    echo "> 현재 구동 중인 애플리케이션이 없으므로 종료하지 않습니다."
@@ -33,11 +31,11 @@ fi
 echo "> 새 애플리케이션 배포"
 JAR_NAME=$(ls -tr $REPOSITORY/*.jar | tail -n 1)
 
-echo "> JAR Name : $JAR_NAME"
+echo "> JAR Name: $JAR_NAME"
 
 # Jar 파일은 실행 권한이 없는 상태
 # nohup으로 실행할 수 있게 실행 권한 부여
-echo "> JAR_NAME에 실행권한 추가"
+echo "> $JAR_NAME에 실행권한 추가"
 chmod +x $JAR_NAME
 
 echo "> $JAR_NAME 실행"
