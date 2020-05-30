6cf1548a80954c279320c14a88f0f4400c26cb4e
monkeyDugi <lbd4946@gmail.com>
2020-05-30 17:49:07 +0900

    jar 실행 스크립트 수정

--- script/deploy.sh
+++ script/deploy.sh
@@ -55,7 +55,7 @@ echo "> $JAR_NAME 실행"
 #   -> application-real.properties의 spring.profiles.inclue=oauth, real-db 옵션 때문에
 #      real-bd 역시 함께 활성화 대상에 포함
 nohup java -jar \
-      -Dspring.config.location=classpath:/application.properties,classpath:/application-real.properties,/home/ec2-user/app/monkey-music-service/application-oauth.properties,/home/ec2-user/app/monkey-music-service/application-real-db.properties \
+      -Dspring.config.location=classpath:/application-real.properties,/home/ec2-user/app/monkey-music-service/application-oauth.properties,/home/ec2-user/app/monkey-music-service/application-real-db.properties \
       -Dspring.profiles.active=real \
       # nohup 실행 시 CodeDeploy는 무한 대기
       # 이 이슈 해결을 위해 nohup.out 파일을 표준 입출력용으로 별도로 사용
