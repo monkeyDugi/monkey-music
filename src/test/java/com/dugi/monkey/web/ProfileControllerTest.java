f88be4569e53b48ca8586bdd81666e5c235179c3
monkeyDugi <lbd4946@gmail.com>
2020-05-31 02:33:55 +0900

    profile컨트롤러 테스트

--- src/test/java/com/dugi/monkey/web/ProfileControllerTest.java
+++ src/test/java/com/dugi/monkey/web/ProfileControllerTest.java
@@ -1,21 +1,27 @@
 package com.dugi.monkey.web;
 
+import com.dugi.monkey.scheduler.DailyChartScheduler;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class ProfileControllerTest {
-
-    @LocalServerPort
-    private int port;
 
     @Autowired
     private TestRestTemplate restTemplate;
+
+    @MockBean
+    private DailyChartScheduler dailyChartScheduler;
 
     @Test
     public void profile은_인증없이_호출된다() throws Exception {
