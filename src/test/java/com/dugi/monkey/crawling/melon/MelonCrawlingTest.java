23de41d9c269838216f2997a9de28596ccdb68c2
monkeyDugi <lbd4946@gmail.com>
2020-05-31 03:12:51 +0900

    api 쿼터 소모하는 테스트 주석

--- src/test/java/com/dugi/monkey/crawling/melon/MelonCrawlingTest.java
+++ src/test/java/com/dugi/monkey/crawling/melon/MelonCrawlingTest.java
@@ -1,26 +1,21 @@
 package com.dugi.monkey.crawling.melon;
 
-import org.junit.Test;
-import java.io.IOException;
-
-import static org.assertj.core.api.Assertions.assertThat;
-
 public class MelonCrawlingTest {
 
-    private MelonCrawling melonCrawling = new MelonCrawling();
-
-    @Test
-    public void 멜론_크롤링_결과는_NULL이_아니다() throws IOException {
-        assertThat(melonCrawling.getMelonChartConnection()).isNotNull();
-    }
-
-    @Test
-    public void 멜론_크롤링_결과의_일간차트는_100개_이다() throws IOException {
-        assertThat(melonCrawling.getEntireElementsOfMelonChart().size()).isEqualTo(100);
-    }
-
-    @Test
-    public void 멜론_크롤링_일간차트에서_10개를_추출한다() {
-        assertThat(melonCrawling.getCrawlingResult(10).size()).isEqualTo(10);
-    }
+//    private MelonCrawling melonCrawling = new MelonCrawling();
+//
+//    @Test
+//    public void 멜론_크롤링_결과는_NULL이_아니다() throws IOException {
+//        assertThat(melonCrawling.getMelonChartConnection()).isNotNull();
+//    }
+//
+//    @Test
+//    public void 멜론_크롤링_결과의_일간차트는_100개_이다() throws IOException {
+//        assertThat(melonCrawling.getEntireElementsOfMelonChart().size()).isEqualTo(100);
+//    }
+//
+//    @Test
+//    public void 멜론_크롤링_일간차트에서_10개를_추출한다() {
+//        assertThat(melonCrawling.getCrawlingResult(10).size()).isEqualTo(10);
+//    }
 }
