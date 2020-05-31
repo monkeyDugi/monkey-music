0deb042ad97d7fc6e7bf45e5335ee5e4de6a527b
monkeyDugi <lbd4946@gmail.com>
2020-05-31 11:11:06 +0900

    api key 변경

--- src/main/java/com/dugi/monkey/crawling/youtube/APIKey.java
+++ src/main/java/com/dugi/monkey/crawling/youtube/APIKey.java
@@ -7,7 +7,7 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public enum APIKey {
     
-    API_KEY("AIzaSyC8OSK4rWi-la7PbAZk3XwE2TIdIf3VY3w");
+    API_KEY("AIzaSyAB-DoSDnW8aWvxv671mAd2bCvfHMjkeoE");
     
     private final String apiKey;
 }
