dfd4a125db21fa5d0ddcda6b8b3937426f2bdcea
monkeyDugi <lbd4946@gmail.com>
2020-06-09 00:16:14 +0900

    암호화 테스트 ok

--- src/test/java/com/dugi/monkey/config/jasyptConfigTest.java
+++ src/test/java/com/dugi/monkey/config/jasyptConfigTest.java
@@ -3,16 +3,21 @@ package com.dugi.monkey.config;
 import org.jasypt.encryption.pbe.StandardPBEStringEncryptor;
 import org.junit.Test;
 
+//@RunWith(SpringRunner.class)
+//@SpringBootTest
 public class jasyptConfigTest {
 
     @Test
-    public void test() {
+    public void test() throws Exception {
         StandardPBEStringEncryptor standardPBEStringEncryptor = new StandardPBEStringEncryptor();
 
         standardPBEStringEncryptor.setAlgorithm("PBEWithMD5AndDES");
         standardPBEStringEncryptor.setPassword("monkeyDugi");
+//        standardPBEStringEncryptor.setStringOutputType("base64");
 
-        String des = standardPBEStringEncryptor.decrypt("goUwYcn43JyzLCVpX9HB81T6QtSQ2FG4sNudAjhm371RDPQGUJvdbkxOvkn5SpEp");
+//        String enc = standardPBEStringEncryptor.encrypt("이병덕");
+        String des = standardPBEStringEncryptor.decrypt("ugw17ozluAaUYlnuPPf0kWUQlVhO5qqV");
+//        System.out.println("enc = " + enc);
         System.out.println("des = " + des);
     }
 }
