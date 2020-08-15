0faf822fad13cbf790038711572bbc4c945793f9
monkeyDugi <lbd4946@gmail.com>
2020-08-15 23:49:07 +0900

    공백 제거

--- src/main/java/com/dugi/monkey/web/searchchart/SearchChartController.java
+++ src/main/java/com/dugi/monkey/web/searchchart/SearchChartController.java
@@ -20,7 +20,7 @@ import java.util.List;
 
 @RequiredArgsConstructor
 @RestController
-public class     SearchChartController {
+public class SearchChartController {
 
     private final SearchChartService searchChartService;
 
